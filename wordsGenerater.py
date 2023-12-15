aea9a7a8a22689e5833329c2d7d5e41afb7a620d
Muaeen <muaeen.gharib@hotmail.com>
2023-12-15 20:35:00 +0400

    Generate names

---
+++ wordsGenerater.py
@@ -0,0 +1,17 @@
+# Importing the Faker library
+from faker import Faker
+
+# Creating an instance of the Faker class to generate fake data
+fake = Faker()
+
+# Using list comprehension to create a list of 10 fake first names
+first_names = [fake.first_name() for _ in range(10)]
+print(first_names)
+
+# Defining the file name where the names will be saved
+file_path = "names.txt"
+
+# Opening the file in write mode
+with open(file_path, 'w') as f:
+    for name in first_names:
+        f.write(name + "\n") # Writing each name to the file followed by a newline
