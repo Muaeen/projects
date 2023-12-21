ab75d5de7255d91553fc0fa274f6a6e87ab44686
Muaeen <muaeen.gharib@hotmail.com>
2023-12-21 23:31:51 +0400

    -

--- main.py
+++ main.py
@@ -11,3 +11,33 @@ authenticator.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(authenticator, wait_on_rate_limit=True)
 
+
+try:
+    tweet = api.update_status('Your tweet content here')
+    print("Tweet created successfully")
+except tweepy.Forbidden as e:
+    print("Forbidden error: Check your access level and permissions.")
+    print(e)
+except tweepy.TweepyException as e:
+    print("An error occurred: ", e)
+
+
+
+
+
+
+# query = 'python'
+# num_tweets = 100
+
+# tweets = tweepy.Cursor(api.search_tweets, q= query).items(num_tweets)
+
+# username = 'iimuaeen'
+
+# try:
+#     user = api.get_user(screen_name=username)
+#     print(f"Username: {user.screen_name}")
+# except tweepy.errors.Forbidden as e:
+#     print("Access Forbidden:", e)
+# except tweepy.errors.TweepyException as e:  # A more general exception
+#     print("Tweepy Exception:", e)
+
